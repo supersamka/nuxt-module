c4b44dd885ee089d841f9462b42cfe152fde3354
Sergey Bedritsky <yo@gmail.com>
2020-02-27 23:58:20 +0200

    weeeee

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export default function SimpleModule(moduleOptions) {
   console.log('MODULE STUFF', moduleOptions);
+  console.log('THIS IS MY CONTRIBUTION');
 }
 
 // REQUIRED if publishing the module as npm package
